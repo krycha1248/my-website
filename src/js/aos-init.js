82cb25e4ff527e133ea85866ac366de0330f98ee
Krystian Włodek <krycha1248@gmail.com>
2026-03-18 21:55:15 +0100

    animacje

---
+++ src/js/aos-init.js
@@ -0,0 +1,10 @@
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+document.addEventListener('DOMContentLoaded', () => {
+  AOS.init({
+    duration: 800,
+    easing: 'ease-out',
+    once: false
+  });
+});
